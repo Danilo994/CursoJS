b1255a7dd8f4a745b8dd51210c0f5804ffd8411d
Danilo <daniloarrabal1@hotmail.com>
2023-02-02 19:58:42 -0300

    basico do JS

---
+++ Seção 1/aula003/index.js
@@ -0,0 +1,5 @@
+console.log('Danilo "Arrabal"'); // String = Texto
+console.log("Danilo 'Arrabal'"); // Number = Número
+console.log(`'Danilo' "Arrabal"`);
+
+console.log(15, 15.85, 'Danilo Arrabal');
